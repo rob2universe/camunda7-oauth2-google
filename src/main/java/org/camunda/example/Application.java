fb1e416e274fb9ee37b7c31439029f3586a33045
rob2universe <robert.emsbach@camunda.com>
2022-03-18 15:52:00 +0800

    cleanup

--- src/main/java/org/camunda/example/Application.java
+++ src/main/java/org/camunda/example/Application.java
@@ -3,23 +3,12 @@ package org.camunda.example;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.security.Principal;
 
 @Slf4j
 @SpringBootApplication
 public class Application {
 
-  public static void main(String... args) {
-    SpringApplication.run(Application.class, args);
-  }
-
-  @RequestMapping(value = "/user")
-  public Principal user(Principal principal) {
-
-    log.debug("HERE:" + principal);
-    return principal;
-  }
-
+    public static void main(String... args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
